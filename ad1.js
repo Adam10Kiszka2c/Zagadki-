2454f460801e6bebbdc35d8bb900d4c8e52b54de
Adam10Kiszka2c <81560763+Adam10Kiszka2c@users.noreply.github.com>
2022-02-02 18:23:42 +0100

    Add files via upload

---
+++ ad1.js
@@ -0,0 +1,55 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Zegar";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="Maj";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Globus";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="Cyrk aka SLO";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Deszcz";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
